4d82510ed72b52c19d3ec2add704e0ac016b3fa8
Petrik Diak <diak@petrik.hu>
2025-03-28 11:19:34 +0100

    SavingAccount osztály kész

---
+++ Bank/SavingAccount.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+	internal class SavingAccount : BankAccount
+	{
+
+		static double baseInterest;
+		public double Interest { get; set; }
+
+		public SavingAccount(Owner owner) : base(owner)
+		{
+			Interest= baseInterest;
+		}
+
+		public override bool Withdraw(double amount)
+		{
+			if (Balance - amount >=0)
+			{
+				Balance -= amount;
+				return true;
+			}
+			return false;
+		}
+		public void AddInterest()
+		{
+			Balance += Balance * (Interest/100);
+		}
+	}
+}
